243f2d21dd29d4d54c8497c48f1ef98ea9e9f4de
Diego Lemus <diego.lemus@iteso.mx>
2026-02-02 12:47:53 -0600

    primera parte

---
+++ app.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+import logic
+
+window = tk.Tk()
+window.title("Calculadora Logica")
+
+expression = ""
+
+def add(letter):
+    global expression
+    expression+=letter
+    display.delete(0,tk.END)
+    display.insert(0,expression)
+
+def clear():
+    global expression
+    expression = ""
+    display.delete(0,tk.END)
+
+def backspace():
+    global expression
+    if expression == "":
+        return
+    expression = expression[:-1]
+    display.delete(0,tk.END)
+    display.insert(0,expression)
+
+
+
+display = tk.Entry(window, width=40)
+display.pack(pady=10)
+
+#Variables
+boton_p = tk.Button(window, text="p", width=2, command=lambda: add("p"))
+boton_p.pack(side=tk.LEFT , padx=5)
+
+boton_q = tk.Button(window, text="q", width=2, command=lambda: add("q"))
+boton_q.pack(side=tk.LEFT , padx=5)
+
+boton_r = tk.Button(window, text="r", width=2, command=lambda: add("r"))
+boton_r.pack(side=tk.LEFT , padx=5)
+
+#Boton de Borrar Todo
+boton_clear = tk.Button(window, text="C",width=2, command=lambda:clear())
+boton_clear.pack(side=tk.LEFT, padx=5)
+
+#Boton de regresar 1
+button_backspace = tk.Button(window, text="⌫", width=2, command=lambda:backspace())
+button_backspace.pack(side=tk.LEFT,padx=5)
+
+#Operadores Logicos 
+boton_conjunction = tk.Button(window, text="∧",width=2, command=lambda:add('∧'))
+boton_conjunction.pack(side=tk.LEFT, padx=5)
+
+boton_disyunction = tk.Button(window, text="∨",width=2, command=lambda:add('∨'))
+boton_disyunction.pack(side=tk.LEFT, padx=5)
+
+boton_negation = tk.Button(window, text="¬",width=2, command=lambda:add('¬'))
+boton_negation.pack(side=tk.LEFT, padx=5)
+
+boton_entonces = tk.Button(window, text="→",width=2, command=lambda:add('→'))
+boton_entonces.pack(side=tk.LEFT, padx=5)
+
+boton_entonces = tk.Button(window, text="↔",width=2, command=lambda:add('↔'))
+boton_entonces.pack(side=tk.LEFT, padx=5)
+
+
+boton_entonces = tk.Button(window, text="(",width=2, command=lambda:add('( '))
+boton_entonces.pack(side=tk.LEFT, padx=5)
+
+boton_entonces = tk.Button(window, text="(",width=2, command=lambda:add(' )'))
+boton_entonces.pack(side=tk.LEFT, padx=5)
+
+
+
+
+
+
+window.mainloop()
---
+++ logic.py
@@ -0,0 +1,65 @@
+import re
+
+
+def segregate(expr_internal: str) -> list[str]:
+    expr_internal = expr_internal.split()
+    return expr_internal
+
+def validar_parentesis(segregacion: list[str]) -> bool:
+    counter = 0
+
+    for i in range(len(segregacion)):
+        if counter > 0:
+            if segregacion[i] == "(":
+                counter+=1
+            elif segregacion[i] == ")":
+                counter-=1
+        else: return False
+
+    return counter == 0
+
+def encontrar_variables(segregacion: list[str]) -> list[str]:
+    total_vars = ['p', 'q', 'r']
+    seg_set = set(segregacion)
+    vars_used = {var for var in total_vars if var in seg_set}
+    return vars_used
+
+def definir_variables(variables: list[str]) -> dict[str, str]:
+
+    var_def = {}
+    for var in variables:
+        str_boolean_val = str(input(f'Enter "True" "False" for {var}: ')).lower().capitalize()
+        
+
+        while str_boolean_val not in ('True','False'):
+            str_boolean_val = str(input(f'ONLY Enter "True" or "False" for {var}: ')).lower().capitalize()
+
+
+        var_def[var] = str_boolean_val
+    return var_def
+
+
+def traducir_proposicion(proposicion:str, variables: dict[str, str]) -> bool:
+    print(proposicion)
+    for var, val in variables.items():
+        proposicion = re.sub(rf"\b{var}\b",val,proposicion)
+        print(f"variable {var} reformada")
+        print(proposicion)
+    return proposicion
+        
+
+
+
+proposicion = "not ( p and q ) or r"
+
+segregacion = segregate(proposicion)
+print(segregacion)
+
+validador = validar_parentesis(segregacion)
+
+variables = encontrar_variables(segregacion)
+variables_definidas = {'r': 'True', 'q': 'False', 'p': 'True'}
+print(variables_definidas)
+print("==========================================\n")
+traduccion = traducir_proposicion(proposicion,variables_definidas)
+print(traduccion)
